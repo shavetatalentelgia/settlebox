cb3e365557fe865b3f66824e7dfbd42ebdc4c182
shaveta aggarwal <shaveta.aggarwal@talentelgia.com>
2025-12-05 12:22:49 +0530

    add required plugin vendor folders

---
+++ wp-content/plugins/the-post-grid/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'radiustheme/the-post-grid',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1a3a4ec49828227b8c0dea238a54ccbd3ff48ff0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'radiustheme/the-post-grid' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1a3a4ec49828227b8c0dea238a54ccbd3ff48ff0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
